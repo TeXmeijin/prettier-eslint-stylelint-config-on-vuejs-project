c03f1bf0eadaf2a8eabe0526437343a0364c897a
TeXmeijin <i8858garden@gmail.com>
2021-04-18 22:28:28 +0900

    initial commit

---
+++ vue/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>heading</h1>
+    <p>This sentence is over 80 characters. aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+    <p>
+      This sentence is over 120 characters. aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    </p>
+    <p>{{ label }}</p>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extends({
+  data() {
+    return {
+      label: 'hello',
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+div {
+  font-size: .9rem;
+}
+</style>
